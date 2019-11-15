3ba2d2c9310bf65ca695826379797084db66b0e3
95rade <rade95@gmail.com>
2019-11-15 12:57:55 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 docker run --rm \
-           -e USERNAME="Ipatios Asmanidis" \
-           --net=host \
-           -v "$PWD/output":/output \
-           -v "$PWD/suites":/suites \
+           -v "$PWD/tests":/tests \
            -v "$PWD/scripts":/scripts \
            -v "$PWD/reports":/reports \
            --security-opt seccomp:unconfined \
